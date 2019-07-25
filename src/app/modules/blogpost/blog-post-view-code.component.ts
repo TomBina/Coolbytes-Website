7263ce086f9ea354c0549c7bcc60dc5ea3657d08
Tom Bina <31348448+TomBina@users.noreply.github.com>
2019-07-25 20:21:43 +0200

    code links improvements

--- src/app/modules/blogpost/blog-post-view-code.component.ts
+++ src/app/modules/blogpost/blog-post-view-code.component.ts
@@ -5,24 +5,15 @@ import { ExternalLink } from "../../../app/services/blogpostservice/external-lin
     selector: "home-blog-post-view-code",
     template: `
         <div>
-            <ng-container *ngIf="!large">
-                <a mat-raised-button [href]="gitHubLink" target="_blank" *ngIf="gitHubLink">view github</a>
-                <a mat-raised-button [href]="sandBoxLink" target="_blank" *ngIf="sandBoxLink">edit/run</a>
-            </ng-container>
-            <ng-container *ngIf="large">
-                <a mat-raised-button [href]="gitHubLink" target="_blank" *ngIf="gitHubLink">view on github</a>
-                <a mat-raised-button [href]="sandBoxLink" target="_blank" *ngIf="sandBoxLink">edit on codesandbox</a>
-            </ng-container>
+            <a mat-raised-button [href]="gitHubLink" target="_blank" *ngIf="gitHubLink">view on github</a>
+            <a mat-raised-button [href]="sandBoxLink" target="_blank" *ngIf="sandBoxLink">edit on codesandbox</a>
         </div>
     `,
-    styleUrls: ["./blog-post-view-code.component.scss"]
+    styles: ["a { margin-right:10px; }"]
 })
 export class BlogPostViewCodeComponent {
     @Input()
     codeLinks: ExternalLink[];
-
-    @Input()
-    large;
 
     findLink(query) {
         let link = this.codeLinks.find(r => r.url.includes(query));
