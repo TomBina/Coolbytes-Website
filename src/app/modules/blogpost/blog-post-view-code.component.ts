73da76e68f428b5714e253ea22eedb8f30c387b0
Tom <tombina@outlook.com>
2019-08-22 17:59:51 +0200

    fix code links text

--- src/app/modules/blogpost/blog-post-view-code.component.ts
+++ src/app/modules/blogpost/blog-post-view-code.component.ts
@@ -5,6 +5,19 @@ import { ExternalLink } from "../../../app/services/blogpostservice/external-lin
     selector: "home-blog-post-view-code",
     template: `
         <div>
+            <ng-container *ngIf="message">
+                <h2>Get the code</h2>
+                <ng-container *ngIf="gitHubLink && sandBoxLink">
+                    <p>
+                        View all code on GitHub, or use codesandbox to edit and run it.
+                    </p>
+                </ng-container>
+                <ng-container *ngIf="gitHubLink && !sandBoxLink">
+                    <p>
+                        View all code on GitHub.
+                    </p>
+                </ng-container>
+            </ng-container>
             <a mat-raised-button [href]="gitHubLink" target="_blank" *ngIf="gitHubLink">view on github</a>
             <a mat-raised-button [href]="sandBoxLink" target="_blank" *ngIf="sandBoxLink">edit on codesandbox</a>
         </div>
@@ -15,10 +28,13 @@ export class BlogPostViewCodeComponent {
     @Input()
     codeLinks: ExternalLink[];
 
+    @Input()
+    message;
+
     findLink(query) {
         let link = this.codeLinks.find(r => r.url.includes(query));
 
-        return (link ? link.url : null);
+        return link ? link.url : null;
     }
 
     get sandBoxLink() {
